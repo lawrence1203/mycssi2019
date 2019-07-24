58e9f12fd2e30ca9c37e13bd61d49b13b7d11cc1
lawrence1203 <santa077@csusm.edu>
2019-07-24 12:00:19 -0700

    js day 1

---
+++ WeLearn/M2-JavaScript/L1-JSIntro/script.js
@@ -0,0 +1,18 @@
+console.log("script running!");
+
+let name = 'Charlie'
+console.log('Hello ' + name + '!');
+let age = 15;
+console.log('You are ' + age + ' years old!');
+
+age = 16
+
+if (age >= 16) {
+  console.log('I can drive')
+} else {
+   console.log('I cannot drive')
+}
+
+const functionName = () => {
+
+}
