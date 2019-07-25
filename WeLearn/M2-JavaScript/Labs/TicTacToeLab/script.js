61f9f263c8918d0edce9195b608cee60d7abdc3e
lawrence1203 <santa077@csusm.edu>
2019-07-25 16:11:32 -0700

    tic tac toe lab

---
+++ WeLearn/M2-JavaScript/Labs/TicTacToeLab/script.js
@@ -0,0 +1,10 @@
+let game = ['-1,-1,-1,-1,-1,-1,-1,-1,-1'];
+let turn - ['p1,p2'];
+
+let box = document.querySelector('.box')
+
+//main vvvv
+
+box.addEventListener('click', (event) => {
+ box.innerHTML('x')
+});
